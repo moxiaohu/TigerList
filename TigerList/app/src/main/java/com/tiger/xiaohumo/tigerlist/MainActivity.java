6d66b2945c6a670e218857773f5f82127b97091b
moxiaohu <xiaohumo.efrei@gmail.com>
2015-12-04 09:42:18 +0100

    test5

--- TigerList/app/src/main/java/com/tiger/xiaohumo/tigerlist/MainActivity.java
+++ TigerList/app/src/main/java/com/tiger/xiaohumo/tigerlist/MainActivity.java
@@ -31,7 +31,7 @@ public class MainActivity extends Activity {
     @Bind(R.id.input_dream)
 
 
-            
+
     EditText edtTxtInputDream;
 
     public static ArrayList<DreamObject> list;
@@ -69,6 +69,8 @@ public class MainActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+
+        // hello how are you~~~
     }
 
     @Override
